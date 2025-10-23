f6d94193dd4bfa08360ad5c161f95ba6a1aae8db
Abdul kadir <121648908+Abduk-kadir@users.noreply.github.com>
2025-10-23 11:34:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express=require('express')
+//const express=require('express')
 const app=express()
 require('dotenv').config()
 const dbConfig=require('./config/dbConfig')
@@ -15,5 +15,6 @@ app.use('/api/bookings',bookingRoute)
 
 //const port =process.env.port||5000
 app.listen(port,()=>console.log(`Node app is listinng${port}`))
+
 
 
